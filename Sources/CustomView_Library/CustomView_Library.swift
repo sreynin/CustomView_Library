865c3057f08a7c047b57b27340afd49bacf8e4f6
sreinin <bosreynin7@gmail.com>
2023-09-13 13:21:13 +0700

    fixed code

--- Sources/CustomView_Library/CustomView_Library.swift
+++ Sources/CustomView_Library/CustomView_Library.swift
@@ -76,7 +76,7 @@ public class customView:UIView {
     
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
+    public override func draw(_ rect: CGRect) {
         //Drawing code
         dashedBorder.path  = UIBezierPath(roundedRect: rect, cornerRadius: cornerRadius).cgPath
     }
