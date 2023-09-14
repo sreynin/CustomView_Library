405f630af040ad34b691a61019efafa2af878a5d
sreinin <bosreynin7@gmail.com>
2023-09-14 13:19:04 +0700

    add call profileVC

--- Sources/CustomView_Library/ProfileViewController.swift
+++ Sources/CustomView_Library/ProfileViewController.swift
@@ -7,7 +7,8 @@
 
 import UIKit
 
-public class ProfileViewController: UIViewController {
+public class ProfileViewController: UIViewController,Instantiable {
+    static var storyboardName: StringConvertible { StoryboardName.ProfileSB}
     
     public override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,14 +17,6 @@ public class ProfileViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+  
 
 }
