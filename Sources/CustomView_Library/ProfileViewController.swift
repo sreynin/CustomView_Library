7a227b3a5fe83968fb24315b50f5732d094fd0f2
sreinin <bosreynin7@gmail.com>
2023-09-19 10:04:46 +0700

    Update profile design

--- Sources/CustomView_Library/ProfileViewController.swift
+++ Sources/CustomView_Library/ProfileViewController.swift
@@ -10,14 +10,9 @@ import SDWebImage
 public class ProfileViewController: UIViewController,Instantiable {
     public static var storyboardName: StringConvertible { StoryboardName.SettingSB}
     //MARK: - IBOutlet -
-    @IBOutlet weak var editBtn: UIButton!
+  
     @IBOutlet weak var profileImgView: UIImageView!
-    @IBOutlet weak var nameTF: UITextField!
-    @IBOutlet weak var birthTF: UITextField!
-    @IBOutlet weak var phoneTF: UITextField!
-    @IBOutlet weak var submitBtn: UIButton!
-    
-    
+
     //MARK: - View lifeCycle -
     public override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,22 +22,13 @@ public class ProfileViewController: UIViewController,Instantiable {
     @IBAction func backBtnAction(_ sender: UIButton) {
         self.navigationController?.popViewController(animated: true)
     }
-    
-    @IBAction func submitBtnAction(_ sender: UIButton) {
-       
-        CustomView_Library().showAlert(title:"Submit data successfully!🎉🎊", message: ""){
-            
-        }
-        
-    }
 }
 
 //MARK: - Cusotm function -
 extension ProfileViewController {
     func setup(){
-        self.editBtn.layer.cornerRadius = 10
         self.profileImgView.clipsToBounds = true
-        self.profileImgView.layer.cornerRadius  = 20
+        self.profileImgView.layer.cornerRadius  = 70
         self.profileImgView.sd_setImage(with: URL(string: "https://hips.hearstapps.com/hmg-prod/images/close-up-of-blossoming-rose-flower-royalty-free-image-1580853844.jpg"))
     }
 }
